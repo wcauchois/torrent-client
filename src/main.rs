68a9bd608844d063cda5c0b8d1bbc2e99626d145
Bill Cauchois <wcauchois@gmail.com>
2022-11-08 21:42:31 -0500

    simple torrent file reading

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+use serde_bencode::de;
+use serde_bytes::ByteBuf;
+use std::io::{self, Read};
+
+mod torrent;
+
+fn main() {
+    let mut f = std::fs::File::open("debian.torrent").unwrap();
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).unwrap();
+    println!("read {} bytes", buffer.len());
+
+    match de::from_bytes::<torrent::Torrent>(&buffer) {
+        Ok(t) => torrent::render_torrent(&t),
+        Err(e) => println!("ERROR: {:?}", e),
+    };
+}
+
+/*
 #[tokio::main]
 async fn main() {
     println!("hello");
 }
+*/
---
+++ src/torrent.rs
@@ -0,0 +1,91 @@
+use serde::Deserialize;
+use serde_bencode::de;
+use serde_bytes::ByteBuf;
+use std::io::{self, Read};
+
+// Pretty much just stolen from:
+// https://github.com/toby/serde-bencode/blob/master/examples/parse_torrent.rs
+
+#[derive(Debug, Deserialize)]
+pub struct Node(String, i64);
+
+#[derive(Debug, Deserialize)]
+pub struct File {
+    path: Vec<String>,
+    length: i64,
+    #[serde(default)]
+    md5sum: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Info {
+    name: String,
+    pieces: ByteBuf,
+    #[serde(rename = "piece length")]
+    piece_length: i64,
+    #[serde(default)]
+    md5sum: Option<String>,
+    #[serde(default)]
+    length: Option<i64>,
+    #[serde(default)]
+    files: Option<Vec<File>>,
+    #[serde(default)]
+    private: Option<u8>,
+    #[serde(default)]
+    path: Option<Vec<String>>,
+    #[serde(default)]
+    #[serde(rename = "root hash")]
+    root_hash: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Torrent {
+    info: Info,
+    #[serde(default)]
+    announce: Option<String>,
+    #[serde(default)]
+    nodes: Option<Vec<Node>>,
+    #[serde(default)]
+    encoding: Option<String>,
+    #[serde(default)]
+    httpseeds: Option<Vec<String>>,
+    #[serde(default)]
+    #[serde(rename = "announce-list")]
+    announce_list: Option<Vec<Vec<String>>>,
+    #[serde(default)]
+    #[serde(rename = "creation date")]
+    creation_date: Option<i64>,
+    #[serde(rename = "comment")]
+    comment: Option<String>,
+    #[serde(default)]
+    #[serde(rename = "created by")]
+    created_by: Option<String>,
+}
+
+pub fn render_torrent(torrent: &Torrent) {
+    println!("name:\t\t{}", torrent.info.name);
+    println!("announce:\t{:?}", torrent.announce);
+    println!("nodes:\t\t{:?}", torrent.nodes);
+    if let Some(al) = &torrent.announce_list {
+        for a in al {
+            println!("announce list:\t{}", a[0]);
+        }
+    }
+    println!("httpseeds:\t{:?}", torrent.httpseeds);
+    println!("creation date:\t{:?}", torrent.creation_date);
+    println!("comment:\t{:?}", torrent.comment);
+    println!("created by:\t{:?}", torrent.created_by);
+    println!("encoding:\t{:?}", torrent.encoding);
+    println!("piece length:\t{:?}", torrent.info.piece_length);
+    println!("private:\t{:?}", torrent.info.private);
+    println!("root hash:\t{:?}", torrent.info.root_hash);
+    println!("md5sum:\t\t{:?}", torrent.info.md5sum);
+    println!("path:\t\t{:?}", torrent.info.path);
+    if let Some(files) = &torrent.info.files {
+        for f in files {
+            println!("file path:\t{:?}", f.path);
+            println!("file length:\t{}", f.length);
+            println!("file md5sum:\t{:?}", f.md5sum);
+        }
+    }
+}
